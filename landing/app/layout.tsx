78b37f24b8f7dd7aa0bda31d353687efffa6944e
Robin Dang <robin.dang@live.se>
2024-09-03 00:29:20 +0200

    next

---
+++ landing/app/layout.tsx
@@ -0,0 +1,22 @@
+import { ThemeModeScript } from "flowbite-react";
+import { Inter } from "next/font/google";
+import { type FC, type PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const RootLayout: FC<PropsWithChildren> = function ({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <ThemeModeScript />
+      </head>
+      <body className={twMerge("bg-gray-50 dark:bg-gray-900", inter.className)}>
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ landing/tailwind.config.ts
@@ -0,0 +1,85 @@
+import flowbite from "flowbite/plugin";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./app/**/*.{ts,tsx}", "./node_modules/flowbite-react/lib/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
+        theme: {
+          50: "#F4E7FF",
+          100: "#E8CFFF",
+          200: "#D1AFFF",
+          300: "#BB8FFF",
+          400: "#A46FFF",
+          500: "#8A45FF",
+          600: "#7338D4",
+          700: "#5D2CAB",
+          800: "#461F82",
+          900: "#301259",
+        },
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        body: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "system-ui",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        mono: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+    },
+  },
+  plugins: [flowbite],
+};
+
+export default config;
